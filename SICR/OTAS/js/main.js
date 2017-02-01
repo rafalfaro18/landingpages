a9bf96a86d183b45330b8aa1c03efc924593bd89
Rafael Alfaro <webmaster@profimercadeo.com>
2017-02-01 15:12:31 -0600

    Actualiza Pipes al cambiar tamaño de la pantalla

--- SICR/OTAS/js/main.js
+++ SICR/OTAS/js/main.js
@@ -1,6 +1,8 @@
 function detect_mobile() {
 	if(window.innerWidth >= 768) {
 		for(var i=0;i<document.getElementsByClassName('pipes').length;i++){document.getElementsByClassName('pipes')[i].style.display='inherit';}	
+	} else{
+		for(var i=0;i<document.getElementsByClassName('pipes').length;i++){document.getElementsByClassName('pipes')[i].style.display='none';}
 	}
 }
 
@@ -10,15 +12,25 @@ function doOnOrientationChange()
     {  
       case -90:
       case 90:
-        detect_mobile()
+        detect_mobile();
         break; 
       default:
-        detect_mobile()
+        detect_mobile();
         break; 
     }
   }
 
   window.addEventListener('orientationchange', doOnOrientationChange);
 
-  // Initial execution if needed
-  //doOnOrientationChange();
+var addEvent = function(object, type, callback) {
+    if (object == null || typeof(object) == 'undefined') return;
+    if (object.addEventListener) {
+        object.addEventListener(type, callback, false);
+    } else if (object.attachEvent) {
+        object.attachEvent("on" + type, callback);
+    } else {
+        object["on"+type] = callback;
+    }
+};
+
+addEvent(window, "resize", detect_mobile);
